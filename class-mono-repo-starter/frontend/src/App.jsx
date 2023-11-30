5aaa5fff3d19c5cfdab38a38b117460bf5380678
So Youn Choi <manghatuhyo@gmail.com>
2023-11-30 09:37:31 +0100

    to find out external styling affecting it or not. i guess i dont have css that affecting it but this text is left-centered so i want to find out why

--- class-mono-repo-starter/frontend/src/App.jsx
+++ class-mono-repo-starter/frontend/src/App.jsx
@@ -1,17 +1,3 @@
-import { BrowserRouter, Routes, Link } from "react-router-dom";
-import routes from "./routes/routes";
-import { userStore } from "./stores/userStore";
-import "./App.css";
-
 export const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <main>
-          {/* <Routes>{routes}</Routes> */}
-          <Routes>{routes}</Routes>
-        </main>
-      </BrowserRouter>
-    </>
-  );
+  return "you are alone in dark ... "
 };
