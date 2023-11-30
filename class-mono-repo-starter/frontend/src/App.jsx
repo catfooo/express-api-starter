523a48c41bd51cefc54ef2cc71f122c1f7392e9f
So Youn Choi <manghatuhyo@gmail.com>
2023-11-30 11:27:14 +0100

    using useeffect in order to not use seperate css or seperate setting for certain styling, such a small thing so i didnt wanted to have specific structure for this. but sometimes i feel... isnt using useeffect for this becomes more much than other thing? but just happy to give a try

--- class-mono-repo-starter/frontend/src/App.jsx
+++ class-mono-repo-starter/frontend/src/App.jsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+
 export const App = () => {
+  useEffect(() => {
+    document.body.style.textAlign = 'center'
+
+    // Cleanup function to reset the style when the component unmounts
+    return () => {
+      document.body.style.textAlign = ''
+    }
+  }, []) // the empty dependency array ensures this effect runs only once when the component mounts
   return "you are alone in dark ... "
 };
