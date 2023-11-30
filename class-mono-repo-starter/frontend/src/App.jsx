72ec5198a4cf02bd371cec1d901a99fa69ec171f
So Youn Choi <manghatuhyo@gmail.com>
2023-11-30 11:51:50 +0100

    to use roboto

--- class-mono-repo-starter/frontend/src/App.jsx
+++ class-mono-repo-starter/frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { useEffect } from "react";
 export const App = () => {
   useEffect(() => {
     document.body.style.textAlign = 'center'
+    document.body.style.fontFamily = 'Roboto, sans-serif'
 
     // Cleanup function to reset the style when the component unmounts
     return () => {
       document.body.style.textAlign = ''
+      document.body.style.fontFamily = ''
     }
   }, []) // the empty dependency array ensures this effect runs only once when the component mounts
   return "you are alone in dark ... "
